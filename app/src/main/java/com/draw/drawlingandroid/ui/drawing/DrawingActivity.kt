907e1cd54c50762ba1fe26f57e4b933a95cbf05c
Giga99 <igorstevanovic99@gmail.com>
2022-09-02 03:20:32 +0200

    DrawingView setup

--- app/src/main/java/com/draw/drawlingandroid/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/draw/drawlingandroid/ui/drawing/DrawingActivity.kt
@@ -1,10 +1,14 @@
 package com.draw.drawlingandroid.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.draw.drawlingandroid.R
 import com.draw.drawlingandroid.databinding.ActivityDrawingBinding
+import com.draw.drawlingandroid.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,10 +29,29 @@ class DrawingActivity : AppCompatActivity() {
         }
     }
 
+    private fun selectColor(color: Int) {
+        binding.drawingView.setColor(color)
+        binding.drawingView.setThickness(Constants.DEFAULT_PAINT_THICKNESS)
+    }
+
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launchWhenStarted {
             viewModel.selectedColorButtonId.collect { id ->
                 binding.colorGroup.check(id)
+                when(id) {
+                    R.id.rbBlack -> selectColor(Color.BLACK)
+                    R.id.rbBlue -> selectColor(Color.BLUE)
+                    R.id.rbGreen -> selectColor(Color.GREEN)
+                    R.id.rbOrange -> selectColor(
+                        ContextCompat.getColor(this@DrawingActivity, R.color.orange)
+                    )
+                    R.id.rbRed -> selectColor(Color.RED)
+                    R.id.rbYellow -> selectColor(Color.YELLOW)
+                    R.id.rbEraser -> {
+                        binding.drawingView.setColor(Color.WHITE)
+                        binding.drawingView.setThickness(40f)
+                    }
+                }
             }
         }
     }
