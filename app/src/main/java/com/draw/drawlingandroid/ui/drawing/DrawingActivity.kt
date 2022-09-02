eec09b64effed5caf05b0424c18a6273e94b0ae4
Giga99 <igorstevanovic99@gmail.com>
2022-09-02 02:47:04 +0200

    Custom RadioButtons

--- app/src/main/java/com/draw/drawlingandroid/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/draw/drawlingandroid/ui/drawing/DrawingActivity.kt
@@ -1,8 +1,9 @@
 package com.draw.drawlingandroid.ui.drawing
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.draw.drawlingandroid.databinding.ActivityDrawingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,9 +12,24 @@ class DrawingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDrawingBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val viewModel: DrawingViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityDrawingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        subscribeToUiStateUpdates()
+
+        binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
+            viewModel.checkRadioButton(checkedId)
+        }
+    }
+
+    private fun subscribeToUiStateUpdates() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.selectedColorButtonId.collect { id ->
+                binding.colorGroup.check(id)
+            }
+        }
     }
 }
