3a3a3a94de0f6c12abf279cdf998dd3e74d482bb
Giga99 <igorstevanovic99@gmail.com>
2022-09-05 23:14:38 +0200

    Added serbian wordlist

--- app/src/main/java/com/draw/drawlingandroid/data/remote/ws/WordList.kt
+++ app/src/main/java/com/draw/drawlingandroid/data/remote/ws/WordList.kt
@@ -2,11 +2,13 @@ package com.draw.drawlingandroid.data.remote.ws
 
 enum class WordList(val uiValue: String) {
     PROGRAMMERS_WORDLIST("Programmers Word List"),
+    SERBIAN_WORDLIST("Serbian Word List"),
     DEFAULT_WORDLIST("Default Word List");
 
     companion object {
         fun fromUiValue(str: String) = when (str) {
             PROGRAMMERS_WORDLIST.uiValue -> PROGRAMMERS_WORDLIST
+            SERBIAN_WORDLIST.uiValue -> SERBIAN_WORDLIST
             else -> DEFAULT_WORDLIST
         }
     }
